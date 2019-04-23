7427392dce1f74605b2ef37e1c69ec97cbe61551
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-04-22 20:31:52 -0500

    routes

--- CpuScheduling/src/app/modules/routing/app-routing.module.ts
+++ CpuScheduling/src/app/modules/routing/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlgorithmSelectionComponent } from 'src/app/components/algorithm-selection/algorithm-selection.component';
+import { SjfInputFormComponent } from '../shortest-job-first/components/forms/sjf-input-form/sjf-input-form.component';
+import { SjfSimulationComponent } from '../shortest-job-first/components/sjf-simulation/sjf-simulation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AlgorithmSelectionComponent
+  },
+  {
+    path: 'sjf-form',
+    component: SjfInputFormComponent
+  },
+  {
+    path: 'sjf',
+    component: SjfSimulationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
