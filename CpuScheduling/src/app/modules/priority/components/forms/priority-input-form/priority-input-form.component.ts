82ee53a7cb652fa40ad921c3d147b76be6530c5c
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-04-27 13:49:38 -0500

    Priority input

--- CpuScheduling/src/app/modules/priority/components/forms/priority-input-form/priority-input-form.component.ts
+++ CpuScheduling/src/app/modules/priority/components/forms/priority-input-form/priority-input-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-priority-input-form',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./priority-input-form.component.scss']
 })
 export class PriorityInputFormComponent implements OnInit {
+  processes: { name: string; burstTime: number; arrivalTime: number; priority: number }[] = [
+    {
+      name: 'P1',
+      arrivalTime: 0,
+      burstTime: 1,
+      priority: 1
+    }
+  ];
+  preemtive = false;
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  add() {
+    this.processes.push({
+      name: `P${this.processes.length + 1}`,
+      arrivalTime: 0,
+      burstTime: 1,
+      priority: 1
+    });
   }
 
+  remove(i: number) {
+    this.processes.splice(i, 1);
+  }
+
+  priority() {
+    this.router.navigate(['priority'], {
+      queryParams: {
+        processes: JSON.stringify(this.processes),
+        preemtive: this.preemtive
+      }
+    });
+  }
 }
--- CpuScheduling/src/app/modules/priority/priority.module.ts
+++ CpuScheduling/src/app/modules/priority/priority.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PriorityInputFormComponent } from './components/forms/priority-input-form/priority-input-form.component';
 import { PrioritySimulationComponent } from './components/priority-simulation/priority-simulation.component';
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatCheckboxModule
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PriorityInputFormComponent, PrioritySimulationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    MatCheckboxModule
   ]
 })
-export class PriorityModule { }
+export class PriorityModule {}
